b58996aecb88ec15e4204e35ae942c17ab0773da
AlexandrePhilibertCPNV <alexandre.philibert@cpnv.ch>
2021-12-13 16:12:44 +0100

    Move DashboardPage to modules

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -1,37 +1,3 @@
-import React from "react";
+import DashboardPage from "src/modules/dashboard/DashboardPage";
 
-// components
-
-import CardLineChart from "src/components/Cards/CardLineChart";
-import CardBarChart from "src/components/Cards/CardBarChart";
-import CardPageVisits from "src/components/Cards/CardPageVisits";
-import CardSocialTraffic from "src/components/Cards/CardSocialTraffic";
-
-// layout for page
-
-import Admin from "src/layouts/Admin";
-
-export default function Dashboard() {
-  return (
-    <>
-      <div className="flex flex-wrap">
-        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          <CardLineChart />
-        </div>
-        <div className="w-full xl:w-4/12 px-4">
-          <CardBarChart />
-        </div>
-      </div>
-      <div className="flex flex-wrap mt-4">
-        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          <CardPageVisits />
-        </div>
-        <div className="w-full xl:w-4/12 px-4">
-          <CardSocialTraffic />
-        </div>
-      </div>
-    </>
-  );
-}
-
-Dashboard.layout = Admin;
+export default DashboardPage;
