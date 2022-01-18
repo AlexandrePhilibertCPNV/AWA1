3ecc847aae33ddfec65480ac0655c10825a5abd4
AlexandrePhilibertCPNV <alexandre.philibert@cpnv.ch>
2022-01-18 20:34:11 +0100

    Add Auth layout background animation

--- src/layouts/Auth.tsx
+++ src/layouts/Auth.tsx
@@ -1,4 +1,5 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useEffect } from "react";
+import Head from "next/head";
 
 // components
 
@@ -6,16 +7,125 @@ import Navbar from "src/components/Navbars/AuthNavbar";
 import FooterSmall from "src/components/Footers/FooterSmall";
 
 export default function Auth({ children }: PropsWithChildren<{}>) {
+  useEffect(() => {
+    particlesJS("particles-js", {
+      particles: {
+        number: {
+          value: 80,
+          density: {
+            enable: true,
+            value_area: 700,
+          },
+        },
+        color: {
+          value: "#ffffff",
+        },
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 0,
+            color: "#000000",
+          },
+          polygon: {
+            nb_sides: 5,
+          },
+        },
+        opacity: {
+          value: 0.5,
+          random: false,
+          anim: {
+            enable: false,
+            speed: 0.1,
+            opacity_min: 0.1,
+            sync: false,
+          },
+        },
+        size: {
+          value: 3,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 10,
+            size_min: 0.1,
+            sync: false,
+          },
+        },
+        line_linked: {
+          enable: true,
+          distance: 150,
+          color: "#ffffff",
+          opacity: 0.4,
+          width: 1,
+        },
+        move: {
+          enable: true,
+          speed: 2,
+          direction: "none",
+          random: false,
+          straight: false,
+          out_mode: "out",
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200,
+          },
+        },
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onhover: {
+            enable: true,
+            mode: "grab",
+          },
+          onclick: {
+            enable: true,
+            mode: "push",
+          },
+          resize: true,
+        },
+        modes: {
+          grab: {
+            distance: 140,
+            line_linked: {
+              opacity: 1,
+            },
+          },
+          bubble: {
+            distance: 400,
+            size: 40,
+            duration: 2,
+            opacity: 8,
+            speed: 3,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+          push: {
+            particles_nb: 4,
+          },
+          remove: {
+            particles_nb: 2,
+          },
+        },
+      },
+      retina_detect: true,
+    });
+  }, []);
+
   return (
     <>
+      <Head>
+        <script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js" />
+      </Head>
       <Navbar />
       <main>
-        <section className="relative w-full h-full py-40 min-h-screen">
+        <section className="relative w-full h-full min-h-screen py-40">
           <div
-            className="absolute top-0 w-full h-full bg-blueGray-800 bg-no-repeat bg-full"
-            style={{
-              backgroundImage: "url('/img/register_bg_2.png')",
-            }}
+            id="particles-js"
+            className="absolute top-0 w-full h-full bg-blueGray-800"
           ></div>
           {children}
           <FooterSmall absolute />
