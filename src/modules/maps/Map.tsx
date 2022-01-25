1525ce3eee717da2c7c9187ec88cfe2113e57506
AlexandrePhilibertCPNV <alexandre.philibert@cpnv.ch>
2022-01-25 22:25:45 +0100

    Handle currently selected state color on Map

--- src/modules/maps/Map.tsx
+++ src/modules/maps/Map.tsx
@@ -2,18 +2,29 @@ import USA from "src/assets/usa";
 import { SVGMap } from "react-svg-map";
 
 import "react-svg-map/lib/index.css";
+import { useState } from "react";
 
 type MapProps = {
   onLocationClick?: (location: any) => void;
 };
 
 export const Map = ({ onLocationClick }: MapProps) => {
+  const [selectedLocation, setSelectedLocation] = useState(null);
+
+  function handleLocationClick(location: any) {
+    setSelectedLocation(location.target.id);
+
+    if (onLocationClick) {
+      onLocationClick(location);
+    }
+  }
+
   return (
     <SVGMap
-      onLocationClick={onLocationClick}
+      onLocationClick={handleLocationClick}
       map={USA}
       locationClassName="fill-current text-blue-300 outline-none hover:text-blue-400 cursor-pointer"
-      isLocationSelected={(location: any) => true}
+      isLocationSelected={(location: any) => location.id === selectedLocation}
     />
   );
 };
