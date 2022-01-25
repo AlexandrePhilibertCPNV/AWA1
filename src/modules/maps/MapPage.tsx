6e73675d6716f283a6f0a77ba965775c9d664688
AlexandrePhilibertCPNV <alexandre.philibert@cpnv.ch>
2022-01-25 21:55:37 +0100

    Dynamically load cities in right card

--- src/modules/maps/MapPage.tsx
+++ src/modules/maps/MapPage.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 
 import { Map } from "./Map";
 import USA from "../../assets/usa";
@@ -6,65 +6,11 @@ import USA from "../../assets/usa";
 import Admin from "src/layouts/Admin";
 import { SalesDonutChart } from "./SalesDonutChart";
 import { SalesBarChart } from "./SalesBarChart";
+import { useStateStats } from "./useStateStats";
 
 const MapPage = () => {
   const [selectedState, setSelectedState] = useState<string>();
-
-  const stats = useMemo(
-    () => [
-      { id: "ak", data: [76, 24] },
-      { id: "ar", data: [24, 76] },
-      { id: "az", data: [75, 23] },
-      { id: "ca", data: [63, 37] },
-      { id: "co", data: [17, 83] },
-      { id: "ct", data: [47, 53] },
-      { id: "dc", data: [84, 16] },
-      { id: "de", data: [84, 16] },
-      { id: "fl", data: [35, 65] },
-      { id: "ga", data: [42, 58] },
-      { id: "hi", data: [63, 37] },
-      { id: "ia", data: [91, 9] },
-      { id: "id", data: [57, 43] },
-      { id: "il", data: [27, 73] },
-      { id: "in", data: [76, 24] },
-      { id: "ks", data: [86, 14] },
-      { id: "ky", data: [57, 43] },
-      { id: "la", data: [48, 52] },
-      { id: "ma", data: [50, 50] },
-      { id: "md", data: [25, 75] },
-      { id: "me", data: [23, 77] },
-      { id: "mi", data: [86, 14] },
-      { id: "mn", data: [25, 75] },
-      { id: "mo", data: [57, 43] },
-      { id: "ms", data: [27, 73] },
-      { id: "mt", data: [76, 24] },
-      { id: "nc", data: [86, 14] },
-      { id: "nd", data: [57, 43] },
-      { id: "ne", data: [48, 52] },
-      { id: "nh", data: [50, 50] },
-      { id: "nj", data: [25, 75] },
-      { id: "nm", data: [23, 77] },
-      { id: "nv", data: [86, 14] },
-      { id: "ny", data: [57, 43] },
-      { id: "oh", data: [27, 73] },
-      { id: "ok", data: [76, 24] },
-      { id: "or", data: [86, 14] },
-      { id: "pa", data: [57, 43] },
-      { id: "ri", data: [48, 52] },
-      { id: "sc", data: [50, 50] },
-      { id: "sd", data: [25, 75] },
-      { id: "tn", data: [23, 77] },
-      { id: "tx", data: [86, 14] },
-      { id: "ut", data: [76, 24] },
-      { id: "va", data: [23, 77] },
-      { id: "vt", data: [63, 37] },
-      { id: "wa", data: [17, 83] },
-      { id: "wi", data: [47, 53] },
-      { id: "wv", data: [84, 16] },
-      { id: "wy", data: [84, 16] },
-    ],
-    []
-  );
+  const { data: stats } = useStateStats(selectedState);
 
   function handleLocationClick(location: any) {
     setSelectedState(location.target.id);
@@ -93,8 +39,7 @@ const MapPage = () => {
                 {
                   backgroundColor: ["#ed64a6", "#eee"],
                   borderColor: ["#ed64a6", "#eee"],
-                  data: stats.find((state) => state.id === selectedState)
-                    ?.data ?? [67, 33],
+                  data: stats?.target ?? [67, 33],
                   fill: false,
                   barThickness: 8,
                 },
@@ -102,12 +47,21 @@ const MapPage = () => {
             />
             <h2 className="my-6 font-bold text-center">Breakdown by city</h2>
             <SalesBarChart
-              labels={["Kansas City", "Saint Louis", "Springfield", "Columbia"]}
+              labels={
+                stats?.cities?.map((c: any) => c.name) ?? [
+                  "Kansas City",
+                  "Saint Louis",
+                  "Springfield",
+                  "Columbia",
+                ]
+              }
               datasets={[
                 {
                   label: "Sales",
                   backgroundColor: "#ed64a6",
-                  data: [23, 67, 34, 45],
+                  data: stats?.cities?.map((c: any) => c.value) ?? [
+                    23, 67, 34, 45,
+                  ],
                 },
               ]}
             />
