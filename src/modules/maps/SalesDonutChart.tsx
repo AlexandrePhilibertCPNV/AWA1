2d2e41779efc3adc92a4afb0313bb4e6936e2f54
AlexandrePhilibertCPNV <alexandre.philibert@cpnv.ch>
2022-01-25 20:21:51 +0100

    Refactor SalesDonutChart to use react-chartjs-2

--- src/modules/maps/SalesDonutChart.tsx
+++ src/modules/maps/SalesDonutChart.tsx
@@ -1,81 +1,71 @@
-import { Chart, ChartConfiguration } from "chart.js";
-import { useEffect } from "react";
+import { Doughnut } from "react-chartjs-2";
 
 type SalesDonutChartProps = {
   datasets: any;
   labels: any;
 };
 
-export const SalesDonutChart = ({ labels, datasets }: SalesDonutChartProps) => {
-  useEffect(() => {
-    const config: ChartConfiguration = {
-      type: "doughnut",
-      data: {
-        labels,
-        datasets,
+export const SalesDonutChart = ({ labels, datasets }: SalesDonutChartProps) => (
+  <Doughnut
+    data={{
+      labels,
+      datasets,
+    }}
+    options={{
+      responsive: true,
+      title: {
+        display: false,
+        text: "Orders Chart",
       },
-      options: {
-        responsive: true,
-        title: {
-          display: false,
-          text: "Orders Chart",
-        },
-        tooltips: {
-          callbacks: {
-            label: function (tooltipItem: any, data: any) {
-              const datasetIndex = tooltipItem.datasetIndex!;
-              const index = tooltipItem.index!;
+      tooltips: {
+        callbacks: {
+          label: function (tooltipItem: any, data: any) {
+            const datasetIndex = tooltipItem.datasetIndex!;
+            const index = tooltipItem.index!;
 
-              const sum = data.datasets![datasetIndex].data.reduce(
-                (acc: number, cur: number) => acc + cur
-              );
-              const value = data.datasets[datasetIndex].data[index];
+            const sum = data.datasets![datasetIndex].data.reduce(
+              (acc: number, cur: number) => acc + cur
+            );
+            const value = data.datasets[datasetIndex].data[index];
 
-              return `${data.labels[index]}: ${Number(
-                (value / sum) * 100
-              ).toFixed(0)}%`;
-            },
+            return `${data.labels[index]}: ${Number(
+              (value / sum) * 100
+            ).toFixed(0)}%`;
           },
         },
-        hover: {
-          mode: "nearest",
-          intersect: true,
-        },
-        legend: {
-          labels: {
-            fontColor: "rgba(0,0,0,.4)",
-          },
-          align: "end",
-          position: "bottom",
-        },
-        scales: {
-          xAxes: [
-            {
-              display: false,
-              scaleLabel: {
-                display: false,
-              },
-            },
-          ],
-          yAxes: [
-            {
-              display: false,
-              scaleLabel: {
-                display: false,
-              },
-              gridLines: {
-                display: false,
-              },
-            },
-          ],
-        },
       },
-    };
-    new Chart(
-      document.getElementById("sales-donut-chart") as HTMLCanvasElement,
-      config
-    );
-  }, [datasets, labels]);
-
-  return <canvas id="sales-donut-chart" />;
-};
+      hover: {
+        mode: "nearest",
+        intersect: true,
+      },
+      legend: {
+        labels: {
+          fontColor: "rgba(0,0,0,.4)",
+        },
+        align: "end",
+        position: "bottom",
+      },
+      scales: {
+        xAxes: [
+          {
+            display: false,
+            scaleLabel: {
+              display: false,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            display: false,
+            scaleLabel: {
+              display: false,
+            },
+            gridLines: {
+              display: false,
+            },
+          },
+        ],
+      },
+    }}
+  />
+);
